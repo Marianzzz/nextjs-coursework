c45a86bed7053518c628b2edefc5d0733bb61093
Marianzzz <marian0412510301@gmail.com>
2025-05-13 22:33:56 +0300

    delete fixed

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,7 +8,7 @@ import { logout } from '@/app/actions/auth';
 export default async function Header() {
     const session = await getSession();
     return (
-        <header className="fixed top-0 left-0 w-full z-50 bg-black text-white p-4 shadow-md">
+        <header className="w-full  bg-black text-white p-4 shadow-md">
             <nav className="flex justify-between items-center">
                 <Link href="/" className="relative w-40 h-15 group">
                     <Image
