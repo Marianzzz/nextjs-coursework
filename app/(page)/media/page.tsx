139224061388f292a4e00ec5a904a93b7f56df99
Marianzzz <marian0412510301@gmail.com>
2025-05-16 00:10:48 +0300

    add metadata

--- app/(page)/media/page.tsx
+++ app/(page)/media/page.tsx
@@ -1,6 +1,13 @@
 import { getAllMedia } from "@/app/actions/media";
 import VideoCard from "@/components/video-card";
 
+import type { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+  title: 'Медіа',
+  description: 'Медіа контент команд та подій',
+}
+
 export default async function MediaPage() {
   const media = await getAllMedia();
 
