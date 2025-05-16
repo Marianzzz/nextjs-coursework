198442edd835af6213392e786eda02bff3162404
Marianzzz <marian0412510301@gmail.com>
2025-05-16 20:58:44 +0300

    add verification user for media admin panel

--- app/(main)/media/[id]/page.tsx
+++ app/(main)/media/[id]/page.tsx
@@ -2,15 +2,21 @@ import { deleteMedia, getMediaById } from "@/app/actions/media";
 import MediaEdit from "../components/edit-delete-media";
 import { getAllDisciplines } from "@/app/actions/disciplines";
 import { notFound } from 'next/navigation';
+import { isAdmin } from "@/lib/admin";
+import { redirect } from "next/navigation";
 
 export default async function MediaOperation({ params }: { params: Promise<{ id: string }> }) {
     const { id } = await params;
     const numericId = Number(id);
     const mediaItem = await getMediaById(numericId);
     const disciplines = await getAllDisciplines();
+    const showAdmin = await isAdmin();
 
     if (!mediaItem) {
         notFound();
+    }
+    if (!showAdmin) {
+        redirect('/media');
     }
 
     async function handleDelete() {
