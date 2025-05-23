ca48cc1879f776d0d9b10b4f1bbad9459e40ec41
Marianzzz <marian0412510301@gmail.com>
2025-05-23 13:52:06 +0300

    add middleware for protect api query

---
+++ middleware.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from "next/server";
+import { decrypt } from "@/lib/session";
+
+export async function middleware(req: NextRequest) {
+
+  const session = req.cookies.get("session")?.value;
+  if (!session) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  const payload = await decrypt(session);
+  if (!payload || payload.role !== "admin") {
+    return NextResponse.json({ error: "Forbidden" }, { status: 403 });
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/api/:path*"],
+};
